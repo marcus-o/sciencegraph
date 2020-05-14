6179b5bfe90c89372b24672991a959372a03a00b
marcus-o <marcus@mmp.local>
2020-05-14 16:24:33 -0400

    cosmetics

--- azure_function/http_request/__init__.py
+++ azure_function/http_request/__init__.py
@@ -10,15 +10,20 @@ from bokeh.io import output_file, show
 from bokeh.models import (BoxZoomTool, Circle, HoverTool,
                           MultiLine, Plot, Range1d,
                           ResetTool, WheelZoomTool,
-                          TapTool, OpenURL)
+                          TapTool, OpenURL, ColumnDataSource, HelpTool, Label)
+from bokeh.models.widgets.markups import Div
+from bokeh.layouts import Column
 from bokeh.palettes import Spectral4
-from bokeh.plotting import from_networkx
+from bokeh.plotting import from_networkx, figure
 from bokeh.models.graphs import NodesAndLinkedEdges
 from bokeh.embed import components
 from bokeh.models.callbacks import CustomJS
 from bokeh.palettes import OrRd9, Blues9
 
 import networkx as nx
+
+import numpy as np
+
 
 t = Template("""
 <html style="height:100vh;">
@@ -32,16 +37,16 @@ t = Template("""
     </head>
     <body style="height:100vh; font-family: Helvetica, sans-serif;">
         <div style="height: 100%">
-            <H1>Science Graph. <a href="https://github.com/marcus-o/sciencegraph/">Code on Github.</a> By Marcus 2020.</H1>
+            <H1>Science Graph. <a href="https://github.com/marcus-o/sciencegraph/">Code on Github.</a></H1>
             <form action="/api/http_request">
                 Search the <a href="https://aka.ms/msracad">Microsoft Academic Graph</a>:
                 <input name="query", value="{{ query }}"/>
                 <select name="n">
-                    {% for n in ns %}
-                        {% if n == cn %}
-                            <option selected value="{{ n }}">{{ n }}</option>
+                    {% for o in select_options %}
+                        {% if o.value == so %}
+                            <option selected value="{{ o.value }}">{{ o.label }}</option>
                         {% else %}
-                            <option value="{{ n }}">{{ n }}</option>
+                            <option value="{{ o.value }}">{{ o.label }}</option>
                         {% endif %}
                     {% endfor %}
                 </select>
@@ -51,11 +56,51 @@ t = Template("""
             <div style="height: 70%">
                 {{ div|safe }}
             </div>
-            <div id="showpaper"></div>
         </div>
     </body>
 </html>
 """)
+
+tooltips = """
+    <div style="max-width : 300px">
+            <div><span style="">@type</span></div>
+            <div><span style="font-weight: bold;">@title</span></div>
+            <div><span style="">@authors</span></div>
+            <div><span style="font-weight: bold;">@journal</span></div>
+            <div><span style="font-weight: bold;">@year</span></div>
+            <div><span style=""><a href="https://doi.org/@DOI">@DOI</a></span></div>
+    </div>
+"""
+
+showpaper_content = """
+    <div><span style="">@type</span></div>
+    <div><span style="font-weight: bold;">@title</span></div>
+    <div><span style="">@authors</span></div>
+    <div><span style="font-weight: bold;">@journal</span></div>
+    <div><span style="font-weight: bold;">@year</span></div>
+    <div><span style=""><a target="_blank" href="https://doi.org/@DOI">@DOI</a></span></div>
+"""
+
+code = """
+    if (cb_data.source.selected.indices.length > 0){
+        var selected_index = cb_data.source.selected.indices[0];
+        var tooltip = document.getElementById('showpaper');
+        cb_data.source.data.color[selected_index] = 'grey';
+
+        tooltip.style.display = 'block';
+        tooltip.style.left = '5px';
+        tooltip.style.top = '5px';
+        tooltip.style.width = '500px';
+
+        tp = tp.replace('@type', cb_data.source.data.type[selected_index]);
+        tp = tp.replace('@title', cb_data.source.data.title[selected_index]);
+        tp = tp.replace('@authors', cb_data.source.data.authors[selected_index]);
+        tp = tp.replace('@journal', cb_data.source.data.journal[selected_index]);
+        tp = tp.replace('@year', cb_data.source.data.year[selected_index]);
+        tp = tp.replace('@DOI', cb_data.source.data.DOI[selected_index]);
+        tp = tp.replace('@DOI', cb_data.source.data.DOI[selected_index]);
+        tooltip.innerHTML = tp;
+    }"""
 
 # %%
 cm1 = Blues9
@@ -106,7 +151,7 @@ def evaluate(query, n=100):
         'count': n,
         'offset': '0',
         'orderby': '',
-        'attributes': 'Id,DN,Y,CC,J.JN,AA.AuN,DOI,RId',
+        'attributes': 'Id,DN,Y,CC,J.JN,AA.AuId,AA.DAuN,AA.DAfN,RId,DOI',
         'expr': query,
     })
 
@@ -144,7 +189,7 @@ def prepare_data(query, n):
     # strip incomplete
     papers = [p for p in papers if 'DN' in p.keys()]
     papers = [p for p in papers if 'AA' in p.keys()]
-    # papers = [p for p in papers if 'AuN' in p['AA'].keys()]
+    # papers = [p for p in papers if 'DAuN' in p['AA'].keys()]
     papers = [p for p in papers if 'J' in p.keys()]
     # papers = [p for p in papers if 'JN' in p['JN'].keys()]
     papers = [p for p in papers if 'Y' in p.keys()]
@@ -170,14 +215,14 @@ def prepare_data(query, n):
     # %% get the secondary found papers information
     expr_ref = "Or(Id=" + ",Id=".join([str(rdi) for rdi in rids]) + ")"
     eval_data_ref = evaluate(expr_ref, n=1000)
-    if eval_data_ref is not None:
+    if 'entities' in eval_data.keys():
         # %% process secondary found papers
         papers_ref = [e for e in eval_data_ref['entities']]
 
         # strip incomplete
         papers_ref = [p for p in papers_ref if 'DN' in p.keys()]
         papers_ref = [p for p in papers_ref if 'AA' in p.keys()]
-        # papers = [p for p in papers if 'AuN' in p['AA'].keys()]
+        # papers = [p for p in papers if 'DAuN' in p['AA'].keys()]
         papers_ref = [p for p in papers_ref if 'J' in p.keys()]
         # papers = [p for p in papers if 'JN' in p['JN'].keys()]
         papers_ref = [p for p in papers_ref if 'Y' in p.keys()]
@@ -210,10 +255,10 @@ def prepare_data(query, n):
         color = cm2[int(8*(1-paper['CC']/max_cit))]
         G.add_node(
             id,
-            type='primary',
+            type='Primary Search Result',
             color=color,
             title=paper['DN'],
-            authors=', '.join([a['AuN'] for a in paper['AA']]),
+            authors=', '.join([a['DAuN'] for a in paper['AA']]),
             journal=paper['J']['JN'],
             year=paper['Y'],
             DOI=paper['DOI'],
@@ -223,10 +268,10 @@ def prepare_data(query, n):
         color = cm1[int(8*(1-paper['CC']/max_cit_ref))]
         G.add_node(
             id,
-            type='reference',
+            type='Reference',
             color=color,
             title=paper['DN'],
-            authors=', '.join([a['AuN'] for a in paper['AA']]),
+            authors=', '.join([a['DAuN'] for a in paper['AA']]),
             journal=paper['J']['JN'],
             year=paper['Y'],
             DOI=paper['DOI'],
@@ -249,73 +294,152 @@ def prepare_data(query, n):
     return G, exprs[0]
 
 
-def draw_plot(G, query, expr):
-    plot = Plot(
+def prepare_data_authors(query):
+    # %% convert the natural language request to a query
+    interpret_data = interpret(query)
+    exprs = [
+        e['rules'][0]['output']['value']
+        for e in interpret_data['interpretations']
+        if e['rules'][0]['output']['type'] == 'query']
+
+    # for expr in exprs:
+    #    if 'AA.AuN=' in expr:
+    #        res_expr = expr
+    # else:
+    #    res_expr = exprs[0]
+
+    # %%
+    eval_data = evaluate(exprs[0], n=1000)
+    if 'entities' not in eval_data.keys():
+        return 0, 0
+    # %% process primary found papers
+    papers = [e for e in eval_data['entities']]
+    # strip incomplete
+    papers = [p for p in papers if 'DN' in p.keys()]
+    papers = [p for p in papers if 'AA' in p.keys()]
+    papers = [p for p in papers if 'J' in p.keys()]
+    papers = [p for p in papers if 'Y' in p.keys()]
+    papers = [p for p in papers if 'CC' in p.keys()]
+    for p in papers:
+        if 'DOI' not in p.keys():
+            p['DOI'] = 'unknown'
+    max_cit = max([p['CC'] for p in papers])
+    ids = [p['Id'] for p in papers]
+
+    authors = []
+    [
+        authors.extend(a)
+        for a in [
+            p['AA'] for p in papers]]
+    auids = [a['AuId'] for a in authors]
+    auids, auids_idx, auids_counts = np.unique(
+        auids, return_index=True, return_counts=True)
+    auids = [int(auid) for auid in auids]
+    authors = [authors[idx] for idx in auids_idx]
+
+    G = nx.Graph()
+    # add primary papers
+    for id, paper in zip(ids, papers):
+        color = cm2[int(8*(1-paper['CC']/max_cit))]
+
+        G.add_node(
+            id,
+            type='Publication',
+            color=color,
+            title=paper['DN'],
+            authors=', '.join([a['DAuN'] for a in paper['AA']]),
+            journal=paper['J']['JN'],
+            year=paper['Y'],
+            DOI=paper['DOI'],
+            size=15)
+    # add co-auhors
+    second_max = sorted(auids_counts)[-2]
+    for auid, author, occ in zip(auids, authors, auids_counts):
+        if max(auids_counts) == occ:
+            size = 20
+            color = cm1[0]
+        else:
+            size = 10
+            color = cm1[int(8*(1-occ/second_max))]
+        G.add_node(
+            auid,
+            type='Author',
+            color=color,
+            title=author['DAuN'],
+            authors=author['DAfN'],
+            journal='',
+            year='',
+            DOI='',
+            size=size)
+
+    # add connections from primaries to references
+    for p in papers:
+        if 'AA' in p.keys():
+            G.add_edges_from([(p['Id'], a['AuId']) for a in p['AA']])
+    return G, exprs[0]
+
+
+def draw_plot(G, query, expr, type='publications'):
+    # plot
+    plot = figure(
         x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1),
-        sizing_mode="stretch_both")
-    plot.title.text = "Relationship Graph for: " + expr
+        sizing_mode="stretch_both",
+        tools="")
+    plot.axis.visible = False
+    plot.xgrid.grid_line_color = None
+    plot.ygrid.grid_line_color = None
 
-    tooltips = """
-        <div style="max-width : 300px">
-                <div><span style="">@type</span></div>
-                <div><span style="font-weight: bold;">@title</span></div>
-                <div><span style="">@authors</span></div>
-                <div><span style="font-weight: bold;">@journal</span></div>
-                <div><span style="font-weight: bold;">@year</span></div>
-                <div><span style=""><a href="https://doi.org/@DOI">@DOI</a></span></div>
-        </div>
-    """
+    # legend
+    if type == 'publications':
+        plot.circle(
+            x=[-2, ], y=[-2, ],
+            fill_color=cm2[3], size=20,
+            legend_label='Publication, Color measures Citation Count')
+        plot.circle(
+            x=[-2, ], y=[-2, ],
+            fill_color=cm1[3], size=10,
+            legend_label='Reference, Color measures Citation Count')
+    if type == 'authors':
+        plot.circle(
+            x=[-2, ], y=[-2, ],
+            fill_color=cm2[3], size=15,
+            legend_label='Publication, Color measures Citation Count')
+        plot.circle(
+            x=[-2, ], y=[-2, ],
+            fill_color=cm1[3], size=10,
+            legend_label='Co-Author, Color measures Collaboration')
+    plot.legend.title = 'Visualization for "' + query + '", created using Microsoft Academic Graph and Sciencegraph by Marcus Ossiander, 2020'
 
+    # tools
     node_hover_tool = HoverTool(tooltips=tooltips)
     zoom_tool = WheelZoomTool()
+
+    div = Div(
+        text='<div id="showpaper" style="position: absolute; display: none; width=500px"></div>',
+        name='showpaper', sizing_mode="stretch_width")
     tap_tool_open = TapTool()
-
-    showpaper = """
-        <div style="max-width : 80%">
-                <div><span style="">@type</span></div>
-                <div><span style="font-weight: bold;">@title</span></div>
-                <div><span style="">@authors</span></div>
-                <div><span style="font-weight: bold;">@journal</span></div>
-                <div><span style="font-weight: bold;">@year</span></div>
-                <div><span style=""><a target="_blank" href="https://doi.org/@DOI">@DOI</a></span></div>
-        </div>
-    """
-
-    code = '''  if (cb_data.source.selected.indices.length > 0){
-                    var selected_index = cb_data.source.selected.indices[0];
-                    var tooltip = document.getElementById("showpaper");
-                    cb_data.source.data.color[selected_index] = 'grey'
-                    tp = tp.replace('@type', cb_data.source.data.type[selected_index]);
-                    tp = tp.replace('@title', cb_data.source.data.title[selected_index]);
-                    tp = tp.replace('@authors', cb_data.source.data.authors[selected_index]);
-                    tp = tp.replace('@journal', cb_data.source.data.journal[selected_index]);
-                    tp = tp.replace('@year', cb_data.source.data.year[selected_index]);
-                    tp = tp.replace('@DOI', cb_data.source.data.DOI[selected_index]);
-                    tp = tp.replace('@DOI', cb_data.source.data.DOI[selected_index]);
-                    tooltip.innerHTML = tp;
-            } '''
-
     tap_tool_open.callback = CustomJS(
-        args={'tp': showpaper}, code=code)
-
+        args={'tp': showpaper_content}, code=code)
+    help_tool = HelpTool(
+        help_tooltip='Created using Microsoft Academic Graph and Sciencegraph by Marcus Ossiander, 2020',
+        redirect='https://github.com/marcus-o/sciencegraph/')
     plot.add_tools(
         node_hover_tool,
         zoom_tool,
-        # BoxZoomTool(),
+        BoxZoomTool(),
         ResetTool(),
         tap_tool_open,
-    )
+        help_tool)
     plot.toolbar.active_scroll = zoom_tool
 
+    # graph
     graph_renderer = from_networkx(
         G, nx.spring_layout, scale=1, center=(0, 0), seed=12345)
-
     # normal
     graph_renderer.node_renderer.glyph = Circle(
         size="size", fill_color="color")
     graph_renderer.edge_renderer.glyph = MultiLine(
         line_alpha=0.2)
-
     # selection
     graph_renderer.node_renderer.selection_glyph = Circle(
         fill_color="color", fill_alpha=1, line_alpha=1)
@@ -325,53 +449,71 @@ def draw_plot(G, query, expr):
         fill_color="color", fill_alpha=0.5, line_alpha=0.5)
     graph_renderer.edge_renderer.nonselection_glyph = MultiLine(
         line_alpha=0.2)
-
     # hover
     graph_renderer.node_renderer.hover_glyph = Circle(
         fill_color='#abdda4')
     graph_renderer.edge_renderer.hover_glyph = MultiLine(
         line_color='#abdda4', line_width=3)
-
     graph_renderer.inspection_policy = NodesAndLinkedEdges()
     graph_renderer.selection_policy = NodesAndLinkedEdges()
 
+    # add everything
     plot.renderers.append(graph_renderer)
-
-    script, div = components(plot)
+    script, div = components(Column(children=[plot, div], sizing_mode="stretch_both"))
     return script, div
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
 
-    query = req.params.get('query')
-    n = req.params.get('n')
+        select_options = [
+            {'value': '10', 'label': 'Pub. and Ref., n=10'},
+            {'value': '20', 'label': 'Pub. and Ref., n=20'},
+            {'value': '50', 'label': 'Pub. and Ref., n=50'},
+            {'value': 'A', 'label': 'Co-Authors'}]
 
-    if not n:
-        n = 20
-    else:
-        n = int(n)
-    if n > 100:
-        n = 100
+        query = req.params.get('query')
+        n = req.params.get('n')
 
-    if not query:
-        # get common data
-        query = 'metasurface'
-        metasurface_graph, metasurface_expr = prepare_data(query, n=20)
-        metasurface_plot_script, metasurface_plot_div = draw_plot(
-            metasurface_graph, query, metasurface_expr)
-        return func.HttpResponse(t.render(
-            script=metasurface_plot_script,
-            div=metasurface_plot_div,
-            query='metasurface',
-            ns=["10", "20", "50"],
-            cn=str(n)), headers={'content-type': 'text/html'})
-    else:
-        graph, expr = prepare_data(query, n=n)
-        plot_script, plot_div = draw_plot(graph, query, expr)
-        return func.HttpResponse(t.render(
-            script=plot_script,
-            div=plot_div,
-            query=query,
-            ns=["10", "20", "50"],
-            cn=str(n)), headers={'content-type': 'text/html'})
+        try:
+            if not n:
+                n = '20'
+            n = int(n)
+            if n > 100:
+                n = 100
+            if n < 1:
+                n = 1
+        except Exception:
+            n = 'A'
+
+        if not(n == 'A'):
+            if not query:
+                query = 'metasurface'
+
+            graph, expr = prepare_data(query, n=n)
+            plot_script, plot_div = draw_plot(
+                graph, query, expr, type='publications')
+            return func.HttpResponse(t.render(
+                script=plot_script,
+                div=plot_div,
+                query=query,
+                select_options=select_options,
+                so=str(n)), headers={'content-type': 'text/html'})
+        else:
+            if not query:
+                query = 'federico capasso'
+
+            graph, expr = prepare_data_authors(query)
+            plot_script, plot_div = draw_plot(
+                graph, query, expr, type='authors')
+            return func.HttpResponse(t.render(
+                script=plot_script,
+                div=plot_div,
+                query=query,
+                select_options=select_options,
+                so=str(n)), headers={'content-type': 'text/html'})
+    except Exception:
+        print(Exception)
+        return func.HttpResponse(
+            'something went south', headers={'content-type': 'text/html'})
